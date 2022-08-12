b424aedcf035026e7db68dc15c338c1712915bf6
Ihor Riabko <igorriabko2@gmail.com>
2022-08-12 22:52:23 +0300

    change simpleLightbox  connection

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
+function createGalleryLi(elements) {
+  return elements.map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  }).join('')
+};
+
+const galleryList = createGalleryLi(galleryItems)
+
+gallery.insertAdjacentHTML("beforeend", galleryList);
+
+
+
+const imageWrap = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    animationSpeed: 250,
+});
 console.log(galleryItems);
+
